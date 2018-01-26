894fffcd25bd8424b2f1a81029852d7d87e3d789
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-01-26 11:17:11 +0200

    Add redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,51 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { routerMiddleware } from 'react-router-redux'
-import thunk from 'redux-thunk'
-import createHistory from 'history/createBrowserHistory'
-import rootReducer from '../reducers/reducers'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
+import rootReducer from '../reducers/reducers';
+import { createLogger } from 'redux-logger';
 
 export const history = createHistory();
 
 const initialState = {};
 const enhancers = [];
-const middleware = [
-    thunk,
-    routerMiddleware(history)
+const middlewares = [
+  thunk,
+  routerMiddleware(history),
 ];
 
 if (process.env.NODE_ENV === 'development') {
-    const devToolsExtension = window.devToolsExtension;
+  const devToolsExtension = window.devToolsExtension;
 
-    if (typeof devToolsExtension === 'function') {
-        enhancers.push(devToolsExtension())
-    }
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension())
+  }
+
+  const logger = createLogger({
+    diff: true,
+    logErrors: true,
+    collapsed: false,
+    logger: console,
+    level: {
+      prevState: 'debug',
+      action: 'debug',
+      nextState: 'debug',
+      error: 'error',
+    },
+  });
+
+  middlewares.push(logger);
 }
 
 const composedEnhancers = compose(
-    applyMiddleware(...middleware),
-    ...enhancers
+  applyMiddleware(...middlewares),
+  ...enhancers,
 );
 
 const store = createStore(
-    rootReducer,
-    initialState,
-    composedEnhancers
+  rootReducer,
+  initialState,
+  composedEnhancers,
 );
 
 export default store
