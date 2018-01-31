1bd4901dc221059df0d2c9cc781ab6cd92537ffe
Oleksandr Sachuk <ovsachuk@gmail.com>
2018-01-31 09:20:39 +0200

    Add why-did-you-update

--- src/modules/Root/Root.js
+++ src/modules/Root/Root.js
@@ -4,6 +4,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Main } from '../Main';
 
+
+if (process.env.NODE_ENV !== 'production') {
+  const {whyDidYouUpdate} = require('why-did-you-update');
+  whyDidYouUpdate(React)
+}
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
